42d316c5ded587166eba7235679c0ae67c48eca7
IamBramer <tyler.bramer@gmail.com>
2017-04-13 21:36:38 -0500

    Trying to setup responsive image sizes

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
-var cp          = require('child_process');
-var cleanCSS    = require('gulp-clean-css');
-var uglify      = require('gulp-uglify');
-var pump        = require('pump');
-var imagemin    = require('gulp-imagemin');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var cp = require('child_process');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var imagemin = require('gulp-imagemin');
 var deploy = require("gulp-gh-pages");
+var imageResize = require('gulp-image-resize');
+var rename = require("gulp-rename");
 
-var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
@@ -17,16 +19,18 @@ var messages = {
 /**
  * Build the Jekyll Site
  */
-gulp.task('jekyll-build', function (done) {
+gulp.task('jekyll-build', function(done) {
     browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+    return cp.spawn(jekyll, ['build'], {
+            stdio: 'inherit'
+        })
         .on('close', done);
 });
 
 /**
  * Rebuild Jekyll & do page reload
  */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+gulp.task('jekyll-rebuild', ['jekyll-build'], function() {
     browserSync.reload();
 });
 
@@ -44,42 +48,68 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
-gulp.task('sass', function () {
+gulp.task('sass', function() {
     return gulp.src('_scss/main.scss')
         .pipe(sass({
             includePaths: ['scss'],
             onError: browserSync.notify
         }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+            cascade: true
+        }))
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
         .pipe(gulp.dest('_site/css'))
-        .pipe(browserSync.reload({stream:true}))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
         .pipe(gulp.dest('css'));
 });
 
 /**
  * Minify javascript files (must be run separatly after gulp)
  */
-gulp.task('compress', function(cb){
-  pump([
-        gulp.src('js/*.js'),
-        uglify(),
-        gulp.dest('_site/js')
-    ],
-    cb
-  );
+gulp.task('compress', function(cb) {
+    pump([
+            gulp.src('js/*.js'),
+            uglify(),
+            gulp.dest('_site/js')
+        ],
+        cb
+    );
 });
 
 /**
  * Compress image files
  */
-gulp.task('images', function() {
-    gulp.src('img/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('_site/img'));
+var resizeImageTasks = [];
+[600, 900, 1200, 1800, 2400].forEach(function(size) {
+    var resizeImageTask = 'resize_' + size;
+    gulp.task(resizeImageTask, function() {
+        gulp.src('img/**/*.{jpg,png,gif}')
+            .pipe(imageResize({
+                width: size,
+                upscale: false,
+                quality: 0.8,
+                samplingFactor: [2, 2],
+                noProfile: true,
+                interlace: true,
+            }))
+            .pipe(rename(function(path) {
+                path.basename += '-' + size;
+            }))
+            .pipe(gulp.dest('_site/img'));
+        gulp.src('img/**/*.{svg,ico}')
+            .pipe(imagemin())
+            .pipe(gulp.dest('_site/img'));
+    });
+    resizeImageTasks.push(resizeImageTask);
 });
 
-gulp.task("deploy", ["min"], function () {
+gulp.task('resize', resizeImageTasks);
+
+gulp.task("deploy", ["min"], function() {
     return gulp.src("./_site/**/*")
         .pipe(deploy());
 });
@@ -88,7 +118,7 @@ gulp.task("deploy", ["min"], function () {
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch('_scss/**/*.scss', ['sass']);
     gulp.watch(['*.html', '_layouts/**/*.*', 'projects/**/*.*', 'about/**/*.*', 'words/**/*.*', 'contact/**/*.*', 'grid/*.html', 'typography/*.html'], ['jekyll-rebuild']);
 });
@@ -102,4 +132,4 @@ gulp.task('default', ['browser-sync', 'watch']);
 /**
  * Runs minification and image compression
  */
-gulp.task('min', ['compress', 'images']);
+gulp.task('min', ['compress', 'resize']);
