e9dce84ea66e066e68bbc6c294cce9e083516328
IamBramer <tyler.bramer@gmail.com>
2018-05-02 22:49:11 -0500

    Update 2018-05-03T03:48:56.245Z

--- js/main.js
+++ js/main.js
@@ -160,6 +160,37 @@ $(function() {
         }
     });
 
+    var About = Barba.BaseView.extend({
+        namespace: 'post',
+        onEnter: function() {
+          $('.fade-me').css('visibility','hidden');
+
+
+            // The new Container is ready and attached to the DOM.
+        },
+        onEnterCompleted: function() {
+            // The Transition has just finished.
+            sr.sync();
+            var blogImages;
+
+            blogImages = document.getElementsByTagName('img');
+
+            for(var i = 0; i < blogImages.length; i++) {
+                blogImages[i].onclick = function(){
+                  this.classList.toggle('magnify')
+                };
+            };
+        },
+        onLeave: function() {
+
+          // $('#container').mixItUp('destroy');
+            // A new Transition toward a new page has just started.
+        },
+        onLeaveCompleted: function() {
+            // The Container has just been removed from the DOM.
+        }
+    });
+
     // Don't forget to init the view!
     Home.init();
     Projects.init();
